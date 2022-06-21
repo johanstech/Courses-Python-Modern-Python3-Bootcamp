6039ee320fecebd321ca14965c2ab596da7226f7
Johan Andersson <johanstechs@gmail.com>
2022-06-21 22:41:01 +0200

    Temp

---
+++ 12-Lists/for_loop.py
@@ -0,0 +1,9 @@
+colours = ["purple", "teal", "magenta"]
+
+for c in colours:
+    print(c)
+
+numbers = [1, 2, 3, 4]
+
+for n in numbers:
+    print(n)
---
+++ 12-Lists/while_loop.py
@@ -0,0 +1,13 @@
+colours = ["purple", "teal", "magenta"]
+i = 0
+
+while i < len(colours):
+    print(colours[i])
+    i += 1
+
+numbers = [1, 2, 3, 4]
+j = 0
+
+while j < len(numbers):
+    print(numbers[j])
+    j += 1
